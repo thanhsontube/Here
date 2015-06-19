8fff9eb830de3d2dc37828ec22fcf8ee0409dfde
Sonnt <thanhsontube@gmail.com>
2015-06-19 23:15:21 +0700

    Delay time for Search Places

--- mobile/src/main/java/son/nt/here/fragment/SearchPlaceFragment.java
+++ mobile/src/main/java/son/nt/here/fragment/SearchPlaceFragment.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
@@ -24,6 +26,7 @@ import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +52,8 @@ public class SearchPlaceFragment extends BaseFragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private static final String TAG = "SearchPlaceFragment";
+    private static final int SEARCH_WHAT = 1;
+    private static final int SEARCH_DELAY = 750;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -66,6 +71,7 @@ public class SearchPlaceFragment extends BaseFragment {
     private SmoothProgressBar smoothProgressBar;
 
     private SearchView itemSearch;
+    private MyHandler mSearchDelayHandler;
 
     /**
      * Use this factory method to create a new instance of
@@ -145,7 +151,6 @@ public class SearchPlaceFragment extends BaseFragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
 
         void onSelected(MyPlaceDto myPlaceDto);
@@ -153,6 +158,9 @@ public class SearchPlaceFragment extends BaseFragment {
 
     @Override
     public void initData() {
+        if (mSearchDelayHandler == null) {
+            mSearchDelayHandler = new MyHandler(this);
+        }
 
     }
 
@@ -257,38 +265,11 @@ public class SearchPlaceFragment extends BaseFragment {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                placeSearchManager.load(new TsPlace(getActivity(), newText) {
-                    @Override
-                    public void onStart() {
-                        smoothProgressBar.progressiveStart();
-                        ;
-                    }
-
-                    @Override
-                    public void onSucceed(List<MyPlaceDto> listPlaceSearch) {
-                        Logger.debug(TAG, ">>>" + "onSucceed:" + listPlaceSearch.size());
-
-                        if (listPlaceSearch.size() > 0) {
-                            list.clear();
-                            list.addAll(listPlaceSearch);
-                            adapter.notifyDataSetChanged();
-                        }
-                        smoothProgressBar.progressiveStop();
-                    }
-
-                    @Override
-                    public void onFailed(Throwable error) {
-                        smoothProgressBar.progressiveStop();
-                    }
-
-                    @Override
-                    public void onReservePlaceIdOK(Place place) {
-
-                    }
-                });
+                triggerSearch();
                 return false;
             }
         });
+
     }
 
     @Override
@@ -301,5 +282,58 @@ public class SearchPlaceFragment extends BaseFragment {
         return super.onOptionsItemSelected(item);
     }
 
+    private void triggerSearch () {
+        if (mSearchDelayHandler != null) {
+            mSearchDelayHandler.removeMessages(SEARCH_WHAT);
+            Message message = Message.obtain(mSearchDelayHandler, SEARCH_WHAT);
+            mSearchDelayHandler.sendMessageDelayed(message, SEARCH_DELAY);
+        }
+    }
+
+    private static final class MyHandler extends Handler {
+        final WeakReference<SearchPlaceFragment> contextWeakReference ;
+        public MyHandler (SearchPlaceFragment fragment) {
+            contextWeakReference = new WeakReference<SearchPlaceFragment>(fragment);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            final SearchPlaceFragment f = contextWeakReference.get();
+            if (f == null) {
+                return;
+            }
+            CharSequence newText = f.itemSearch.getQuery();
+            f.placeSearchManager.load(new TsPlace(f.getActivity(), newText.toString()) {
+                @Override
+                public void onStart() {
+                    f.smoothProgressBar.progressiveStart();
+                }
+
+                @Override
+                public void onSucceed(List<MyPlaceDto> listPlaceSearch) {
+                    Logger.debug(TAG, ">>>" + "onSucceed:" + listPlaceSearch.size());
+
+                    if (listPlaceSearch.size() > 0) {
+                        f.list.clear();
+                        f.list.addAll(listPlaceSearch);
+                        f.adapter.notifyDataSetChanged();
+                    }
+                    f.smoothProgressBar.progressiveStop();
+                }
+
+                @Override
+                public void onFailed(Throwable error) {
+                    f.smoothProgressBar.progressiveStop();
+                }
+
+                @Override
+                public void onReservePlaceIdOK(Place place) {
+
+                }
+            });
+
+
+        }
+    }
 
 }
